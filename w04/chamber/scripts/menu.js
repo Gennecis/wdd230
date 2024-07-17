c5bf4593e15673f88c43d82a43762cd373394815
Gennecis <okeziepascal2@gmail.com>
2024-07-17 11:07:42 +0100

    deleted duplicate files

--- w04/chamber/scripts/menu.js
+++
@@ -1,7 +0,0 @@
-const hamButton = document.querySelector('#nav-btn');
-const navigation = document.querySelector('.menu');
-
-hamButton.addEventListener('click', () => {
-	navigation.classList.toggle('open');
-	hamButton.classList.toggle('open');
-});
