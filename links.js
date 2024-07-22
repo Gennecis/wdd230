6d893c4953712ac10811cf58be3a0440a421be77
Gennecis <okeziepascal2@gmail.com>
2024-07-22 15:28:05 +0100

    links.js

--- links.js
+++ links.js
@@ -14,57 +14,27 @@ async function getLinks() {
 
 getLinks()
 
-// function displayLinks(weeks) {
-//     const list = document.querySelector('#activity ul');
-//     list.innerHTML = '';  // Clear the list to avoid duplication
-
-//     weeks.forEach(weekData => {
-//         const listItem = document.createElement('li');
-//         listItem.innerHTML = `<h3>${weekData.week}</h3>`;
-        
-//         weekData.links.forEach(link => {
-//             const linkItem = document.createElement('a');
-//             linkItem.href = link.url;
-//             linkItem.textContent = link.title;
-//             listItem.appendChild(linkItem);
-//             listItem.appendChild(document.createTextNode(' | ')); // Add a separator
-//         });
-
-//         // Remove the last separator
-//         if (listItem.lastChild) {
-//             listItem.removeChild(listItem.lastChild);
-//         }
-
-//         list.appendChild(listItem);
-//     });
-// }
-
-
-
-
 function displayLinks(weeks) {
-    const list = document.querySelector('#activity ul')
-    list.innerHTML = '';
-    // const list = document.createElement('ul');
-    weeks.forEach(weekData => {
-        let listItem = document.createElement('li');
-        listItem.innerHTML = `${weekData.week}:`;
+    const list = document.querySelector('#activity ul');
+    list.innerHTML = '';  // Clear the list to avoid duplication
 
+    weeks.forEach(weekData => {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `<h3>${weekData.week}</h3>`;
+        
         weekData.links.forEach(link => {
-            let anchor = document.createElement('a');
-            let span = document.createElement('span');
-            anchor.setAttribute('href', link.url)
-            anchor.textContent = link.title;
-            span.innerHTML = '&ensp;';
-            listItem.appendChild(anchor);
-            listItem.appendChild(span);
+            const linkItem = document.createElement('a');
+            linkItem.href = link.url;
+            linkItem.textContent = link.title;
+            listItem.appendChild(linkItem);
+            listItem.appendChild(document.createTextNode(' | ')); // Add a separator
         });
 
+        // Remove the last separator
+        if (listItem.lastChild) {
+            listItem.removeChild(listItem.lastChild);
+        }
+
         list.appendChild(listItem);
-
     });
-    
-
-
-    activity.appendChild(list);
 }
